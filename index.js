72ad8794ef2c459496fc73a1ac6afabad31f5688
Ivan Silva <silvaivan280@gmail.com>
2024-12-18 08:28:49 -0300

    agregue los middleware necesarios para el proyecto

--- index.js
+++ index.js
@@ -1,4 +1,7 @@
 import express from "express";
+import cors from "cors";
+import morgan from "morgan";
+import path from "path";
 
 const app = express();
 
@@ -8,3 +11,8 @@ app.listen(app.get('PORT'), () =>{
     console.log("Estoy en el puerto " + app.get('PORT'))
 });
 
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+app.use(cors());
+app.use(morgan("dev"));
+app.use(express.static(path.join(__dirname, '/public')));
