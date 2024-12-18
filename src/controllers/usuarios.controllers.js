de326d80fde8d407be9f5d1afe5f5b1d5ed03b61
Ivan Silva <silvaivan280@gmail.com>
2024-12-18 10:39:13 -0300

    agregue las funciones para obtener un usuario, para loguearse y listar los usuarios

--- src/controllers/usuarios.controllers.js
+++ src/controllers/usuarios.controllers.js
@@ -27,3 +27,55 @@ export const consultaAgregarUsuario = async (req, res) =>{
         });
     }
 }
+
+export  const consultaListarUsuarios = async(req, res) =>{
+    try {
+        const usuarios = await Usuario.find();
+        res.status(200).json(usuarios);
+    } catch (error) {
+        console.log(error);
+        res.status(404).json({
+            mensaje: "Error al intentar obtener los usuarios",
+        })
+    }
+}
+
+export const consultaObtenerUsuario = async (req, res) =>{
+    try {
+        const usuario = await Usuario.findById(req.params.id);
+        res.status(200).json(usuario);
+    } catch (error) {
+        console.log(error);
+        res.status(404).json({
+            mensaje: "Error al intentar obtener el usuario",
+        })
+    }
+}
+
+export const login = async (req, res) =>{
+    try {
+        const {email, password} = req.body;
+        let usuario = await Usuario.findOne({email});
+        if(!usuario){
+            return res.status(404).json({
+                mensaje: "Error, email o password incorrectos",
+            })
+        }
+        const passwordValido = bcryp.compareSync(password, usuario.password);
+        if(!passwordValido){
+            return res.status(404).json({
+                mensaje: "Error, email o password incorrectos",
+            })
+        }
+        res.status(200).json({
+            mensaje: "El usuario es correctos",
+            nombre: usuario.nombre,
+            rol: usuario.rol,
+        })
+    } catch (error) {
+        console.log(error);
+        res.status(404).json({
+            mensaje: "Error, email o password incorrectos",
+        })
+    }
+}
--- src/routes/usuarios.routes.js
+++ src/routes/usuarios.routes.js
@@ -1,8 +1,11 @@
 import { Router } from "express";
+import validarUsuario from "../helpers/validarUsuario";
+import { consultaAgregarUsuario } from "../controllers/usuarios.controllers";
+
 
 const router = Router();
 
 router.route("/usuarios").get();
-router.route("/registro").post();
+router.route("/registro").post(validarUsuario, consultaAgregarUsuario);
 router.route("/login").post();
 router.route("/usuario:id").get();
